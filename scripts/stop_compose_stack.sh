8530f6c64118446eb60728c32d9b40d8ad621e06
Simon Dellenbach <sdellenb@gmail.com>
2021-02-02 12:31:03 +0100

    Don't wipe the database volume unless explicitly stated.
    Some tasks may take longer to finish and persisting the database supports that.

--- scripts/stop_compose_stack.sh
+++ scripts/stop_compose_stack.sh
@@ -6,5 +6,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; # "
 ROOT_DIR="${SCRIPT_DIR%/*}"
 DOCKER_DIR="${ROOT_DIR}/docker"
 
-# Stop the containers and cleanup all volumes.
-docker-compose -f "${DOCKER_DIR}"/docker-compose.yaml down --volumes
+# Stop the containers and cleanup all volumes, if asked to.
+if [[ "$1" == "--cleanup" ]]; then
+    docker-compose -f "${DOCKER_DIR}"/docker-compose.yaml down --volumes
+else
+    docker-compose -f "${DOCKER_DIR}"/docker-compose.yaml down
+fi
