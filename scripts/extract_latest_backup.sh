e51eb624d9025203b4270fa0301a623b1280926e
Simon Dellenbach <sdellenb@gmail.com>
2021-05-15 00:21:50 +0200

    Adjust for wp-config changes in the WordPress image.
    None of the overrides worked anymore in the latest docker image because overriding requires a special wp-config.php, which we now use.
    See https://github.com/docker-library/wordpress/issues/576
    
    The previous values that need to be preserved are now extracted to a environment file for Docker.
    
    A future commit might also include username, password and the database values, which for now stay hard-coded.

--- scripts/extract_latest_backup.sh
+++ scripts/extract_latest_backup.sh
@@ -45,8 +45,21 @@ rm -f extracted/*.sql
 # Source: https://www.wpbeginner.com/wp-tutorials/how-to-move-live-wordpress-site-to-local-server/
 SITEURL=$(awk -F\' '/siteurl/ {print $4}' db_dumps/00-latest.sql)
 TABLE_PREFIX=$(awk -F\' '/table_prefix/ {print $2}' extracted/wp-config.php)
+DB_CHARSET=$(awk -F\' '/DB_CHARSET/ {print $4}' extracted/wp-config.php)
+DB_COLLATE=$(awk -F\' '/DB_COLLATE/ {print $4}' extracted/wp-config.php)
 cat <<EOT >> db_dumps/01-update_siteurl.sql
 UPDATE ${TABLE_PREFIX}options SET option_value = replace(option_value, '${SITEURL}', 'http://localhost:8000') WHERE option_name = 'home' OR option_name = 'siteurl';
 UPDATE ${TABLE_PREFIX}posts SET post_content = replace(post_content, '${SITEURL}', 'http://localhost:8000');
 UPDATE ${TABLE_PREFIX}postmeta SET meta_value = replace(meta_value,'${SITEURL}','http://localhost:8000');
 EOT
+
+# Generate the environment override file from the wp-config values, as that file will be replaced the auto-generated one.
+{
+  printf 'WORDPRESS_TABLE_PREFIX=%s\n' "${TABLE_PREFIX}"
+  printf 'WORDPRESS_DB_CHARSET=%s\n' "${DB_CHARSET}"
+  printf 'WORDPRESS_DB_COLLATE=%s\n' "${DB_COLLATE}"
+
+ } > "${ROOT_DIR}"/docker/wordpress.env
+
+# Delete the old config file to get the auto-generated one provided in the Docker image, which allows for environment variable overrides.
+rm -f extracted/wp-config.php
